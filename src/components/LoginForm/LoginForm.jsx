9226ec7852cf474563b1c5da69c925a4a6fc3d6d
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-07-22 13:57:15 -0500

    added email to sign up

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,43 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+//mui imports
+import { alpha, styled } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import FormControl from '@mui/material/FormControl';
+import Typography from '@mui/material/Typography';
+
+const Input = styled(InputBase)(({ theme }) => ({
+  'label + &': {
+    marginTop: theme.spacing(3),
+  },
+  '& .MuiInputBase-input': {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.mode === 'light' ? '#fcfcfb' : '#2b2b2b',
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    width: 200,
+    padding: '10px 12px',
+    transition: theme.transitions.create([
+      'border-color',
+      'background-color',
+      'box-shadow',
+    ]),
+    '&:focus': {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+    },
+  },
+}));
 
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const errors = useSelector(store => store.errors);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const login = (event) => {
     event.preventDefault();
@@ -25,40 +56,42 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
+    <form className="formPanel" onSubmit={login} >
       {errors.loginMessage && (
         <h3 className="alert" role="alert">
           {errors.loginMessage}
         </h3>
       )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
+      <center>
+        <div>
+          <Typography component="div" variant="h6" color="white" className='form-head form-head-main'> Welcome Back! </Typography>
+        </div>
+        <Input
+          variant="filled"
+          type="text"
+          name="username"
+          required
+          value={username}
+          placeholder="username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </center>
+        <br/>
+      <center>
+      <Input 
+        variant="filled"
+        type="password"
+        name="password"
+        required
+        value={password}
+        placeholder="password"
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      </center>
+        <br/>
+      <center>
+        <input className="btn btn_sizeFull" type="submit" name="submit" value="Sign In" />
+      </center>
     </form>
   );
 }
