f6f60aa81c91a7c0bdf0374596a6b7145090a009
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-07-22 16:18:40 -0500

    added dummy data

--- src/components/MyVideos/MyVideos.jsx
+++ src/components/MyVideos/MyVideos.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, Link, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function MyVideos() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  // this will fetch data on page load
+  // need to get all data from video-responses table
+  useEffect(() => {
+    dispatch({ type: 'FETCH_ALL_VIDEOS' })
+  }, [])
 
   return (
     <div>
-      <h2>My Videos</h2>
+      <h1>My Videos</h1>
 
       for each video 
-        show video.prompt 
-              video.video
-              user.name
+       if video.user.id === user.id
+        show video.prompt_id 
+              video.video_url
+        
 
+      for each video 
+        if video.true = approved tag
+        if video.false = deny tag
+        if video.null = pending tag
+        if video.default = pending tag
+      
     </div>
   );
 }
