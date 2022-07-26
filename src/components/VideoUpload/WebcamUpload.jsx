a551a90d4f739480b47e741e769aa920250e07e4
Badra Mohamed <moha1150@umn.edu>
2022-07-26 14:44:15 -0500

    set up for webcam component

---
+++ src/components/VideoUpload/WebcamUpload.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  ReactMediaRecorder,
+  useReactMediaRecorder,
+} from "react-media-recorder";
+import { useHistory } from "react-router-dom";
+
+function webcamUpload(){
+    return(
+        <>
+        </>
+    )
+}
+export default webcamUpload;
