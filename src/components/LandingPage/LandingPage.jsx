cac47e9c33bcde72d12dcf83fb213c5f987384c9
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-07-27 14:10:10 -0500

    changed landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import './LandingPage.css';
+import { useSelector } from 'react-redux';
 
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
   const history = useHistory();
+  const user = useSelector(store => store.user);
 
   const onLogin = (event) => {
     history.push('/login');
@@ -21,25 +23,34 @@ function LandingPage() {
     <div className="container landing-container">
       <div >
         <center>
-          <h1 className='landing-header'>[logo maybe]</h1>
-          <h1 className='landing-header'>DIGITAL WELLBEING COMPETITION</h1>
+          <h1 className='landing-header'>DIGITAL WELLBEING</h1>
           <p className='landing-copy'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            LiveMore ScreenLess recognizes and appreciates the vast benefits that technology brings to our lives. However, evidence is mounting that misuse and overuse of technology is harmful to the health and wellbeing of young people.
           </p>
-          <Link to="/prompt-page">
-            See All Videos
+          <p className='landing-copy'>
+
+          </p>
+          <Link to="/prompt-page" className='videos-btn'>
+            Watch Videos
           </Link>
           <br/>
           <br/>
-          <button className="btn" onClick={onLogin}>
-            Sign In
-          </button>
+          {!user.id &&
+            <>
+              <b>
+                Register or Sign In
+              <br/>
+                to contribute
+              </b>
+            </>
+          }
 
-          <button className="btn" onClick={onRegister}>
-            Register
-          </button>
-          <h4>Register or Sign in to submit videos of your own</h4>
-
+            {/* <button className="btn" onClick={onLogin}>
+              Sign In
+            </button>
+            <button className="btn" onClick={onRegister}>
+              Register
+            </button> */}
         </center>
       </div>
     </div>
